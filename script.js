8580ea15658c4835e3a2130486eb3776a28e3192
damnsemicolon <shannoninengland@gmail.com>
2022-11-23 22:00:07 +0000

    scripted for lower, upper, numeric & special characters

--- script.js
+++ script.js
@@ -99,33 +99,55 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+//set up
+var pwLength = 0;
+var pwLowerCase = 0;
+var pwUpperCase = 0;
+var pwNumeric = 0;
+var pwSpecial = 0;
+// var userchoices = {
+//   pwLowerCase: lowerCasedCharacters,
+//   pwUpperCase: upperCasedCharacters,
+//   pwNumeric: numericCharacters,
+//   pwSpecial: specialCharacters
+// }
 
 // Function to prompt user for password options
 function getPasswordOptions() {
 
-  // Length of Password (Shortest 11, Longest 64)
-  function lengthOfPWQ () {
-    var lengthOfPWA = prompt("How many characters would you like your password to contain", "Your Number (≥ 10, <65");
-    if (lengthOfPWA >= 10 && lengthOfPWA > 64) {
-      console.log("Good Job");
-    }
-    
-    else if (lengthOfPWA < 10 || lengthOfPWA> 64) {
-      prompt("Please enter number at least or larger than 10 but no more than 64");
+  // Prompt for Length of Password 
+  var pwLength = (prompt("Please enter the length of password:\nBetween 10 and 64"));
+
+while(pwLength < 10 || pwLength > 64) {
+    alert("The password length must be at least 10 characters but no more than 64 characters.");
+    var pwLength = (prompt("Please reenter the length of your password:\nBetween 10 and 64"));
+}
+
+  // Confirm for Lowercase
+  var pwLowerCase = confirm("Does your password needs lowercase characters?\n[OK] = Yes please.\n[Cancel] = No thanks.");
+
+
+  // Confirm for Uppercase
+  var pwUpperCase = confirm("Does your password needs UPPERCASE characters?\n[OK] = Yes please.\n[Cancel] = No thanks.");
+
+  // Confirm for Numeric
+  var pwNumeric = confirm("Does your password needs numbers?\n[OK] = Yes please.\n[Cancel] = No thanks.");
+
+  // Confirm for Special Characters
+  var pwSpecial = confirm("Does your password needs special characters?\n[OK] = Yes please.\n[Cancel] = No thanks.");
+
+  // In case user clicks Cancell for all
+  while(pwLowerCase === false && pwUpperCase === false && pwNumeric === false && pwSpecial === false) {
+    alert("Your password must need at least one criteria from below:\nLowercase Characters\nUppercase Characters\nNumeric Characters\nSpecial Characters");
+    var pwLowerCase = confirm("Does your password needs lowercase characters?\n[OK] = Yes please.\n[Cancel] = No thanks.");
+    var pwUpperCase = confirm("Does your password needs UPPERCASE characters?\n[OK] = Yes please.\n[Cancel] = No thanks.");
+    var pwNumeric = confirm("Does your password needs numbers?\n[OK] = Yes please.\n[Cancel] = No thanks.");
+    var pwSpecial = confirm("Does your password needs special characters?\n[OK] = Yes please.\n[Cancel] = No thanks.");
   }
-  }
 
-  getPasswordOptions();
-  // Prompt for Lowercase
-
-  // Prompt for Uppercase
-
-  // Prompt for Numeric
-
-  // Prompt for Special Characters
+// console.log(boolean(pwLowerCase));
 
 
-  // getPasswordOptions();
 
 // Function for getting a random element from an array
 // function getRandom(arr) {
@@ -146,7 +168,7 @@ function getPasswordOptions() {
 //   var passwordText = document.querySelector('#password');
 
 //   passwordText.value = password;
-}
+
 
 // Add event listener to generate button
 // generateBtn.addEventListener('click', writePassword);
--- testing.js
+++ testing.js
@@ -1,13 +1,165 @@
-
-function getPasswordOptions() {
-
-    //Password Length
-var lengthOfPWA = (prompt("Choose length of your password (10-64 characters)"));
-
-while(lengthOfPWA < 10 || lengthOfPWA > 64) {
-    alert("The password length must be at least 10 characters but no more than 64 characters.");
-    lengthOfPWA = (prompt("Please reenter the length of your password.\n(Between 10 & 64)"));
-}
-
-} //End of function getPasswordOptions
- getPasswordOptions ();
+// Array of special characters to be included in password
+var specialCharacters = [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
+  ];
+  
+  // Array of numeric characters to be included in password
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  
+  // Array of lowercase characters to be included in password
+  var lowerCasedCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  
+  // Array of uppercase characters to be included in password
+  var upperCasedCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
+  
+  // Use composition to make object we can traverse to determine user preferences
+  var characterTypeChoices = {
+    lowercase: lowerCasedCharacters,
+    uppercase: upperCasedCharacters,
+    numeric: numericCharacters,
+    special: specialCharacters
+  }
+  
+  var MIN_PW_LENGTH = 10;
+  var MAX_PW_LENGTH = 64;
+  
+  // Function to prompt user for password options
+  function getPasswordOptions() {
+  
+    var options = {
+      pwLength: 0,
+      characterTypes: []
+    }
+    
+    options.pwLength = prompt("How long would you like your password to be? Must be between " + MIN_PW_LENGTH + " and " + MAX_PW_LENGTH + " characters.");
+  
+    if (options.pwLength < MIN_PW_LENGTH || options.pwLength > MAX_PW_LENGTH) {
+      alert ("Password must be between " + MIN_PW_LENGTH + " and " + MAX_PW_LENGTH);
+      getPasswordOptions();
+    } 
+  
+    // Keep prompting for characterTypes if none have been chosen
+    while (options.characterTypes.length < 1) {
+  
+      // Iterate through choices and prompt user to determine whether each should be included 
+      for (var charType in characterTypeChoices) {
+          if (prompt("Include " + charType + " characters?") !== null) {
+          options.characterTypes.push(characterTypeChoices[charType]);
+        }
+      }
+      if (options.characterTypes.length === 0) {
+        alert("You must include at least one set of characters - please OK an option.")
+      }
+    }
+    return options;
+  }
+  
+  // Function for getting a random element from an array
+  function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+  
+  // Function to generate password with user input
+  function generatePassword() {
+  
+    // Get user's preferences - length and character types
+    var options = getPasswordOptions();
+    var password = "";
+      
+    for (var i = 0; i < options.pwLength; i++) {
+  
+      var charSet = getRandom(options.characterTypes);
+      password = password.concat(getRandom(charSet));
+    }
+    return password;
+  }
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector('#generate');
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+  
+    passwordText.value = password;
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener('click', writePassword);
